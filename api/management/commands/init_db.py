5c53f362fb9a8dbd0c35cadd2d3b6c801b19a162
msadour <sadour.mehdi@gmail.com>
2020-04-26 23:17:27 +0200

    Init database

---
+++ api/management/commands/init_db.py
@@ -0,0 +1,86 @@
+"""
+Command for init the databases.
+"""
+import json
+import os
+
+from django.core.management.base import BaseCommand
+from django.apps import apps
+
+from api.models import *
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class Command(BaseCommand):
+    """
+    Class command.
+    """
+    help = 'Init database'
+
+    def handle(self, *args, **options):
+        """
+        Execute the command that create book(s).
+        """
+
+        def delete_data():
+            BookAdviced.objects.all().delete()
+            Article.objects.all().delete()
+            ImportantMessage.objects.all().delete()
+            Presentation.objects.all().delete()
+            TimeTable.objects.all().delete()
+            Club.objects.all().delete()
+            Member.objects.all().delete()
+            Instructor.objects.all().delete()
+            Course.objects.all().delete()
+            Internship.objects.all().delete()
+            Category.objects.all().delete()
+            Product.objects.all().delete()
+            Order.objects.all().delete()
+            PendingSubscription.objects.all().delete()
+
+        datas_files = BASE_DIR + "/commands/data/database_test.json"
+        # delete_data()
+        with open(datas_files) as json_file:
+            try:
+                datas_list = json.load(json_file)
+                for model, list_datas in datas_list.items():
+                    for datas in list_datas:
+                        if model == 'Club':
+                            time_tables = datas.pop('time_table')
+                            new_object = apps.get_model('api', model)(**datas)
+                            new_object.save()
+                            for time_table in time_tables:
+                                time_table = apps.get_model('api', 'TimeTable')(**time_table)
+                                time_table.save()
+                                new_object.time_table.add(time_table)
+
+                        elif model in ['Course', 'Internship']:
+                            instructor_id = datas.pop('instructor')
+                            instructor = Instructor.objects.get(id=instructor_id)
+                            datas['instructor'] = instructor
+                            time_tables = datas.pop('time_table')
+                            new_object = apps.get_model('api', model)(**datas)
+                            new_object.save()
+
+                            for time_table in time_tables:
+                                time_table = apps.get_model('api', 'TimeTable')(**time_table)
+                                time_table.save()
+                                new_object.time_table.add(time_table)
+
+                        elif model == 'Product':
+                            category_id = datas.pop('category')
+                            category = Category.objects.get(id=category_id)
+                            datas['category'] = category
+                            new_object = apps.get_model('api', model)(**datas)
+                            new_object.save()
+
+                        else:
+                            new_object = apps.get_model('api', model)(**datas)
+                            new_object.save()
+
+            except Exception as e:
+                delete_data()
+                self.stdout.write(e)
+            else:
+                self.stdout.write("The database has been (re)initlialize.")
---
+++ api/serializers.py
@@ -0,0 +1,113 @@
+from rest_framework import serializers
+from .models import *
+
+
+class BookAdvicedSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = BookAdviced
+        fields = ['name', 'author', 'category', 'url']
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Article
+        fields = ['title', 'content', 'category']
+
+
+class ImportantMessageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ImportantMessage
+        fields = ['content', 'date_creation']
+
+
+class PresentationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Presentation
+        fields = ['text']
+
+
+class TimeTableSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TimeTable
+
+
+class ClubSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Club
+        fields = ['name', 'description', 'street', 'number', 'zip_code', 'city', 'country', 'time_table']
+
+
+class MemberSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Member
+        fields = ['id', 'postal_code', 'city', 'street', 'country', 'phone', 'insurance_name', 'insurance_number',
+                  'birthday', 'sex', 'date_creation', 'level']
+
+
+class InstructorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Instructor
+        fields = ['id', 'postal_code', 'city', 'street', 'country', 'phone', 'insurance_name', 'insurance_number',
+                  'birthday', 'sex', 'date_creation', 'level', 'biography']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    instructor = InstructorSerializer(many=False)
+    time_table = TimeTableSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'name', 'description', 'place', 'level', 'category', 'time_table', 'instructor']
+
+
+class InternshipSerializer(serializers.ModelSerializer):
+
+    instructor = InstructorSerializer(many=False)
+    time_table = TimeTableSerializer(many=True)
+
+    class Meta:
+        model = Internship
+        fields = ['id', 'name', 'description', 'place', 'level', 'category', 'time_table', 'instructor',
+                  'date_begin', 'date_end', 'price', 'theme']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    category = CategorySerializer(many=False)
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'price', 'quantity_available', 'size', 'category']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    member = MemberSerializer(many=False)
+    products = ProductSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'member', 'products']
+
+
+class PendingSubscriptionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PendingSubscription
+        fields = ['id', 'email', 'first_name', 'last_name', 'birthday', 'sex', 'date_creation']
+
